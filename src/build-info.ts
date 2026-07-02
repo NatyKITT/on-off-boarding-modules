0429fe52cfe4e2c6657059444201fb320064b647
NatyKITT <natalie.peresoljak@gmail.com>
2026-07-02 13:37:51 +0200

    build version

--- src/build-info.ts
+++ src/build-info.ts
@@ -1,6 +1,6 @@
 // Tento soubor je generován skriptem scripts/build-info.mjs
 // Neupravujte ho ručně.
 
-export const APP_VERSION = "0.2.4";
+export const APP_VERSION = "0.2.5";
 export const APP_BUILD_ENV = "development";
 export const APP_NPM_LIFECYCLE = "build";
